41278f7afe609610b1ceba880ae44d5b1d4e32b0
Muchamad Rijal <merentas5benua27@gmail.com>
2022-10-06 12:29:24 +0700

    Make token & refresh token with Jwt

---
+++ app/config.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+module.exports = {
+    urlDb: process.env.URL_MONGODB_DEV,
+    jwtExpiration: process.env.JWT_EXPIRATION,
+    jwtSecret: process.env.JWT_SECRET_KEY,
+    jwtRefreshExpiration: process.env.JWT_EXPIRATION_REFRESH_TOKEN,
+    jwtRefreshSecret: process.env.JWT_SECRET_KEY_REFRESH_TOKEN,
+    gmail: process.env.EMAIL_GMAIL,
+    password: process.env.PASSWORD_GMAIL,
+}
--- app/utils/jwt.js
+++ app/utils/jwt.js
@@ -1,1 +1,22 @@
 const jwt = require('jsonwebtoken')
+const { jwtExpiration, jwtRefreshExpiration, jwtSecret, jwtRefreshSecret } = require('../config')
+
+const createJwt = ({payload}) => {
+    const token = jwt.sign(payload, jwtSecret, {expiresIn: jwtExpiration})
+    return token
+}
+
+const isTokenValid = ({ token }) => jwt.verify(token, jwtSecret)
+
+const createRefreshJwt = ({payload}) => {
+    const token = jwt.sign(payload, jwtRefreshSecret, {expiresIn: jwtRefreshExpiration})
+    return token
+}
+const isRefreshTokenValid = ({token}) => jwt.verify(token, jwtRefreshSecret)
+
+module.exports = {
+    createJwt,
+    createRefreshJwt,
+    isTokenValid,
+    isRefreshTokenValid
+}
